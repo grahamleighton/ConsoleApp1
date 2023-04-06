408c30c08682d7cc1098f6a61cbf9c399094f157
graha <graha@MSI>
2023-04-06 09:26:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+SampleLibrary.SampleLibrary sl = new SampleLibrary.SampleLibrary();
+
+Console.WriteLine(sl.Add2Numbers(3, 4));
+
+
